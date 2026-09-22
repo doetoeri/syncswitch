cba17adc4ef7a5cedc893628df94180d1259cb5f
doetoeri <doetoeri@goedu.kr>
2026-09-22 23:11:46 +0900

    feat: show numbered review-order badges on selected tapes

---
+++ ci/tapereader-v1.6/TRTapeView.h
@@ -0,0 +1,18 @@
+#import "TRTape.h"
+@class TRTapeView;
+@protocol TRTapeViewDelegate <NSObject>
+- (void)tapeViewTapped:(TRTapeView *)view;
+- (void)tapeViewChanged:(TRTapeView *)view;
+- (void)tapeViewHeld:(TRTapeView *)view;
+@end
+@interface TRTapeView : UIView <UIGestureRecognizerDelegate>
+@property(nonatomic, strong) TRTape *tape;
+@property(nonatomic, weak) id<TRTapeViewDelegate> delegate;
+@property(nonatomic) BOOL editable;
+@property(nonatomic) BOOL focused;
+@property(nonatomic) BOOL selectedForEditing;
+@property(nonatomic) BOOL peelEnabled;
+@property(nonatomic) NSInteger reviewOrder;
+- (void)refresh;
+- (void)togglePeelAnimated;
+@end
